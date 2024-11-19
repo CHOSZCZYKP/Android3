d968d483700a873f4187007ccf88718c9d03a7d3
Pawel <Pawel@PC>
2024-11-19 19:16:02 +0100

    Aplikacja ToDoApp

---
+++ app/src/main/java/com/pb/Android3/TaskListActivity.java
@@ -0,0 +1,11 @@
+package com.pb.Android3;
+
+import androidx.fragment.app.Fragment;
+
+public class TaskListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
